21418de2ed93d517c980e82a20d74a098c1d04a4
Mallikarjun <MallikarjunaBr@global.com>
2022-02-23 21:08:28 +0530

    change hashmap to threadsafeahshmap

--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/cornelk/hashmap v1.0.1 // indirect
+	github.com/dchest/siphash v1.1.0 // indirect
 	github.com/klauspost/compress v1.14.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 )
--- producer.go
+++ producer.go
@@ -3,25 +3,34 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 
 	"github.com/buger/jsonparser"
+	"github.com/cornelk/hashmap"
 	"github.com/valyala/fasthttp"
 )
 
 var dir = "/tmp/big-o/file-%s"
-var cache = make(map[string]string)
+var cache = &hashmap.HashMap{}
 
 func fastHTTPHandlerPut(ctx *fasthttp.RequestCtx) {
 	//os.Remove(fmt.Sprintf(dir, ctx.UserValue("probeId")))
+
 	probeId := ctx.UserValue("probeId")
 	body := ctx.PostBody()
 	fileName := fmt.Sprintf("file-%s", probeId)
 	transmissionTime, _ := jsonparser.GetString(body, "eventTransmissionTime")
-	if savedTransmissionTime, ok := cache[fileName]; ok {
-		fmt.Println("saved" + savedTransmissionTime)
-		fmt.Println("current" + transmissionTime)
+	if savedTransmissionTime, ok := cache.Get(fileName); ok {
 
-		if savedTransmissionTime > transmissionTime {
+		savedInt, _ := strconv.ParseInt(savedTransmissionTime.(string), 10, 64)
+		currentInt, _ := strconv.ParseInt(transmissionTime, 10, 64)
+		fmt.Print("saved ")
+		fmt.Println(savedInt)
+		fmt.Print("current ")
+		fmt.Println(currentInt)
+		fmt.Print("resultbool cmp ")
+
+		if savedInt > currentInt {
 			ctx.Response.SetStatusCode(200)
 			return
 		}
@@ -43,5 +52,5 @@ func fastHTTPHandlerGet(ctx *fasthttp.RequestCtx) {
 }
 
 func writeToCache(fileName string, transmissionTime string) {
-	cache[fileName] = transmissionTime
+	cache.Set(fileName, transmissionTime)
 }
