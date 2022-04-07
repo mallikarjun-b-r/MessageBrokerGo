fe11cf6700222d3bdeeacb0614ea63be9485ef84
root <root@ip-10-0-0-195.ap-south-1.compute.internal>
2022-04-07 15:34:33 +0000

    modify run.sh to run via crontab

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 set -ex
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/usr/local/go/bin
 
-go run . > run.txt 2>&1 &
+mkdir -p /tmp/data
+mount -t ext4 -o defaults,rw,seclabel,noatime,nodiratime,journal_async_commit,nobarrier,data=writeback /dev/nvme1n1 /tmp/data
+rm -rf /tmp/data/lost+found
+
+cd /home/ec2-user/MessageBrokerGo
+pwd
+go run /home/ec2-user/MessageBrokerGo
