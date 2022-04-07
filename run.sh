ab5982141562303c28de82586c7ad9f97fc8fc16
root <root@ip-10-0-0-141.ap-south-1.compute.internal>
2022-04-07 16:12:36 +0000

    Add executable - ran on RHEL

--- run.sh
+++ run.sh
@@ -2,9 +2,8 @@ set -ex
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/usr/local/go/bin
 
 mkdir -p /tmp/data
+umount /tmp/data || true
 mount -t ext4 -o defaults,rw,seclabel,noatime,nodiratime,journal_async_commit,nobarrier,data=writeback /dev/nvme1n1 /tmp/data
 rm -rf /tmp/data/lost+found
 
-cd /home/ec2-user/MessageBrokerGo
-pwd
-go run /home/ec2-user/MessageBrokerGo
+/home/ec2-user/MessageBrokerGo/build/abyss
