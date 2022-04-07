cdf680c94246f0cf0fb42e9662133deac36b4952
root <root@ip-10-0-0-42.ap-south-1.compute.internal>
2022-04-07 16:38:28 +0000

    Add support for multiple FDs and vm swappiness to 95 (agressive)

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 set -ex
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/usr/local/go/bin
 
+ulimit -n 200000
+ulimit -i 200000
+ulimit -u 200000
+
+sysctl -w vm.swappiness=95
+
 mkdir -p /tmp/data
 umount /tmp/data || true
 mount -t ext4 -o defaults,rw,seclabel,noatime,nodiratime,journal_async_commit,nobarrier,data=writeback /dev/nvme1n1 /tmp/data
